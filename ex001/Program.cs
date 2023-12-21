066f38f28bdb57b47e3521a382fd0a9fcceaf731
Khalfina Regina <xalfina_regina@mail.ru>
2023-12-21 17:26:49 +0300

    Add response to task 1

---
+++ ex001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+void PrintNumbers(int m, int n)
+{
+    if (m < n)
+
+    {
+        if (m > 0)
+            Console.Write(m + ", ");
+        PrintNumbers(m + 1, n);
+    }
+
+}
+
+PrintNumbers(-2, 3);
+Console.WriteLine();
